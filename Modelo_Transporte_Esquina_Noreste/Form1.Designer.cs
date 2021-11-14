b0aa52d1989e4649166bbe90ef863ce12c296b78
ingcrislopam <ingcrislopam@gmail.com>
2021-11-14 13:31:00 -0500

    Subiendo proyecto al repositorio

---
+++ Modelo_Transporte_Esquina_Noreste/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Modelo_Transporte_Esquina_Noreste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOrigen = new System.Windows.Forms.TextBox();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnResolver = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtOrigen
+            // 
+            this.txtOrigen.Location = new System.Drawing.Point(130, 32);
+            this.txtOrigen.Name = "txtOrigen";
+            this.txtOrigen.Size = new System.Drawing.Size(100, 20);
+            this.txtOrigen.TabIndex = 0;
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.Location = new System.Drawing.Point(130, 58);
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.Size = new System.Drawing.Size(100, 20);
+            this.txtDestino.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 98);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(573, 207);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(326, 29);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 3;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Origen:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Destino:";
+            // 
+            // btnResolver
+            // 
+            this.btnResolver.Location = new System.Drawing.Point(510, 29);
+            this.btnResolver.Name = "btnResolver";
+            this.btnResolver.Size = new System.Drawing.Size(75, 23);
+            this.btnResolver.TabIndex = 6;
+            this.btnResolver.Text = "Resolver";
+            this.btnResolver.UseVisualStyleBackColor = true;
+            this.btnResolver.Click += new System.EventHandler(this.btnResolver_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 317);
+            this.Controls.Add(this.btnResolver);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtDestino);
+            this.Controls.Add(this.txtOrigen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOrigen;
+        private System.Windows.Forms.TextBox txtDestino;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnResolver;
+    }
+}
+
---
+++ Modelo_Transporte_Esquina_Noreste/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelo_Transporte_Esquina_Noreste
+{
+    public partial class Form1 : Form
+    {
+        DataTable tabla;
+        public Form1()
+        {
+            InitializeComponent();
+            tabla = new DataTable();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            int filas = 0;
+            int columnas = 0;
+            int i = 0;
+            int j = 0;
+            filas = Convert.ToInt32(txtOrigen.Text);
+            columnas = Convert.ToInt32(txtDestino.Text);
+            
+            dataGridView1.DataSource = "";
+            for (i = 0; i < columnas; i++)
+            {
+                if (i == 0)
+                {
+                    tabla.Columns.Add("Base");
+                }
+                tabla.Columns.Add("Destino " + (i + 1));
+            }
+            i = 0;
+            for (j = 0; j < filas; j++)
+            {
+                if (i==0)
+                {
+                    tabla.Rows.Add("Origen " + (j + 1));
+                }
+            }
+            tabla.Rows.Add("Demanda");
+            tabla.Columns.Add("Oferta");
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.DataSource = tabla;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnResolver_Click(object sender, EventArgs e)
+        {
+            int[,] matriz = new int[dataGridView1.Columns.Count + 1, dataGridView1.Rows.Count + 1];
+            int f = 0;
+            int c = 0;
+            for (f = 0; f < dataGridView1.Rows.Count; f++)
+            {
+                for (c = 0; c < dataGridView1.Columns.Count; c++)
+                {
+                    //matriz[c, f] = dataGridView1.Rows[f].Cells[c].Value;   
+                }
+                
+            }
+        }
+    }
+}
